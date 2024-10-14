904e9593b7c76497b35b91e6526ecef67c28c6ae
mudasarasim <117173890+mudasarasim@users.noreply.github.com>
2024-10-14 09:15:06 -0700

    jhk

--- src/pages/student-dashboard.js
+++ src/pages/student-dashboard.js
@@ -40,7 +40,8 @@ const StudentDashboard = () => {
 
                     <li class="nav-item">
                         <a class="nav-link" href="pages/charts/chartjs.html">
-                            <span class="menu-title menuTitle">Educational Games</span>
+                        <i class="mdi mdi-chart menu-icon" style={{ color: '#457B9D' }}></i>
+                        <span class="menu-title menuTitle">Educational Games</span>
                         </a>
                     </li>
 
@@ -78,51 +79,7 @@ const StudentDashboard = () => {
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-xl-12 col-md-6 stretch-card grid-margin grid-margin-sm-0 pb-sm-3">
-                                        <div class="card bg-danger">
-                                            <div class="card-body px-3 py-4">
-                                                <div class="d-flex justify-content-between align-items-start">
-                                                    <div class="color-card">
-                                                        <p class="mb-0 color-card-head">Margin</p>
-                                                        <h2 class="text-white"> $5,300.<span class="h5">00</span>
-                                                        </h2>
-                                                    </div>
-                                                    <i class="card-icon-indicator mdi mdi-cube-outline bg-inverse-icon-danger"></i>
-                                                </div>
-                                                <h6 class="text-white">13.21% Since last month</h6>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-12 col-md-6 stretch-card grid-margin grid-margin-sm-0 pb-sm-3 pb-lg-0 pb-xl-3">
-                                        <div class="card bg-primary">
-                                            <div class="card-body px-3 py-4">
-                                                <div class="d-flex justify-content-between align-items-start">
-                                                    <div class="color-card">
-                                                        <p class="mb-0 color-card-head">Orders</p>
-                                                        <h2 class="text-white"> $1,753.<span class="h5">00</span>
-                                                        </h2>
-                                                    </div>
-                                                    <i class="card-icon-indicator mdi mdi-briefcase-outline bg-inverse-icon-primary"></i>
-                                                </div>
-                                                <h6 class="text-white">67.98% Since last month</h6>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-12 col-md-6 stretch-card pb-sm-3 pb-lg-0">
-                                        <div class="card bg-success">
-                                            <div class="card-body px-3 py-4">
-                                                <div class="d-flex justify-content-between align-items-start">
-                                                    <div class="color-card">
-                                                        <p class="mb-0 color-card-head">Affiliate</p>
-                                                        <h2 class="text-white">2368</h2>
-                                                    </div>
-                                                    <i class="card-icon-indicator mdi mdi-account-circle bg-inverse-icon-success"></i>
-                                                </div>
-                                                <h6 class="text-white">20.32% Since last month</h6>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                 </div>
                             </div>
 
                         </div>
