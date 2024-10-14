5051ac96d037cb72673e418f720b141554c1b97f
mudasarasim <117173890+mudasarasim@users.noreply.github.com>
2024-10-14 05:33:38 -0700

    tyu

--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './login.css';
 
 const ForgotPassword = () => {
-  return <h2>Forgot Password</h2>;
+    return (
+        <div className='main'>
+            <div className='login'>
+                <div class="wrapper fadeInDown">
+                    <div id="formContent">
+
+                        <div class="fadeIn first mt-4">
+                            <h1>Little-Bots</h1>
+                            <p>Having trouble logging in?</p>
+                        </div>
+
+                        <form className='mt-5'>
+                            <label>To reset your password, enter your email.</label>
+                            <input type="email" id="login" class="fadeIn second" name="login" placeholder="Email" />
+                            <input type="submit" class="fadeIn fourth" value="Reset" />
+                        </form>
+
+                        <div id="formFooter">
+                            <Link to={'/'}><p class="underlineHover">Back to Login</p></Link>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ForgotPassword;
