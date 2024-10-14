ac9ba1b808a6c2274a464cef826b74a45ff8c772
mudasarasim <117173890+mudasarasim@users.noreply.github.com>
2024-10-14 09:17:20 -0700

    jhku

--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -17,7 +17,7 @@ const ForgotPassword = () => {
                         <form className='mt-5'>
                             <label>To reset your password, enter your email.</label>
                             <input type="email" id="login" class="fadeIn second" name="login" placeholder="Email" />
-                            <Link to={'/reset-password'}><input class="fadeIn fourth des" value="Reset" /></Link>
+                            <input class="fadeIn fourth des" value="Reset" />
                         </form>
 
                         <div id="formFooter">
