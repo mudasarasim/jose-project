fb694c35bab139e5f18d37e87612f77ab576634f
mudasarasim <117173890+mudasarasim@users.noreply.github.com>
2024-10-14 05:18:30 -0700

    rtyu

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import './login.css';
 
 const Login = () => {
-  return <h2>Login Page</h2>;
+    return (
+        <div className='main'>
+            <div className='login'>
+                <div class="wrapper fadeInDown">
+                    <div id="formContent">
+
+                        <div class="fadeIn first mt-4">
+                            <h1>Little-Bots</h1>
+                            <p>Hello, weolcome back </p>
+                        </div>
+
+                        <form>
+                            <input type="email" id="login" class="fadeIn second" name="login" placeholder="Email" />
+                            <input type="password" id="password" class="fadeIn third" name="login" placeholder="Password" />
+                            <input type="submit" class="fadeIn fourth" value="Sign In" />
+                        </form>
+
+                        <div id="formFooter">
+                            <a class="underlineHover" href="#">Forgot Password?</a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
