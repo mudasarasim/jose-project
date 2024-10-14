d6f80b13111ff649d138fddb5606ab97025a999f
mudasarasim <117173890+mudasarasim@users.noreply.github.com>
2024-10-14 02:46:22 -0700

    llluuu

---
+++ src/pages/forgot-password.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ForgotPassword = () => {
+  return <h2>Forgot Password</h2>;
+};
+
+export default ForgotPassword;
---
+++ src/pages/reset-password.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ResetPassword = () => {
+  return <h2>Reset Password</h2>;
+};
+
+export default ResetPassword;
